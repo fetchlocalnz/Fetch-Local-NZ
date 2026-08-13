b183e4cab4cb4215ca6cb834039ed5de467c4980
fetchlocalnz <fetchlocalnz@gmail.com>
2026-08-13 20:44:52 +1200

    Update manifest.js

--- manifest.js
+++ manifest.js
@@ -1,1 +1,23 @@
-
+export default function manifest() {
+  return {
+    name: "Fetch Local",
+    short_name: "Fetch Local",
+    description: "Find dog training and play buddies near you in NZ.",
+    start_url: "/",
+    display: "standalone",
+    background_color: "#F6EFE3",
+    theme_color: "#C1592C",
+    icons: [
+      {
+        src: "/icon-192.png",
+        sizes: "192x192",
+        type: "image/png",
+      },
+      {
+        src: "/icon-512.png",
+        sizes: "512x512",
+        type: "image/png",
+      },
+    ],
+  };
+}
